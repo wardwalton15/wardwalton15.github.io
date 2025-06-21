9fa71ed1a7aadcf55ec38e851f162d61abb919b9
wardwalton15 <wardwalton15@gmail.com>
2025-06-21 22:59:10 +0000

    Deploying to gh-pages from @ wardwalton15/wardwalton15.github.io@690e1d79920c6d02ab4e95870b135ad15c13edc0 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -23,12 +23,12 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/projects/";
           },
-        },{id: "nav-cv",
-          title: "cv",
+        },{id: "nav-resume",
+          title: "Resume",
           description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
           section: "Navigation",
           handler: () => {
-            window.location.href = "/cv/";
+            window.location.href = "/resume/";
           },
         },{id: "dropdown-bookshelf",
               title: "bookshelf",
